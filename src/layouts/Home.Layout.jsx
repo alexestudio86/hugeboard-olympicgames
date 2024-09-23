536536c4247be66d1bae37bec7f7ca9b930946fc
Alex R <alexestudio86@gmail.com>
2024-09-23 16:58:44 -0600

    Slides in home implementend

--- src/layouts/Home.Layout.jsx
+++ src/layouts/Home.Layout.jsx
@@ -1,7 +1,31 @@
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from 'swiper/modules';
+
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import slide01 from '../assets/slide_01.jpg'
+
 export function HomeLayout() {
-    return(
-        <>
-            Soy el home
-        </>
-    )
+  return (
+    <Swiper
+     navigation={true}
+     className="mySwiper"
+     style={ {height:'90vh'} }
+    >
+        <SwiperSlide>
+            <img src={slide01} alt="slide01" style={ {width: '100%', height: 'auto', objectFit: 'over' } } />
+        </SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+    </Swiper>
+  );
 }
---
+++ src/layouts/Slides.Layout.jsx
@@ -0,0 +1,7 @@
+export function SlidesLayout() {
+    return (
+        <>
+        Soy un slide
+        </>
+    )
+}
